b4abf5a8051307d3a9f3658c62a8c307211b2a03
maliayyildiz <maliayyildiz61@gmail.com>
2021-09-25 18:52:41 +0200

    ecommerce app

---
+++ src/test/java/EcommerceApp/Scenaroi03AddToCart.java
@@ -0,0 +1,14 @@
+package EcommerceApp;
+
+import org.testng.annotations.Test;
+
+public class Scenaroi03AddToCart {
+    @Test
+    public void scenario03(){
+        /*
+        as a user make shop for specific item in the app and add to cart
+        if the item is not on ui(dom) so scroll to until the product and add to cart
+        then verify selected item on the cart
+         */
+    }
+}
